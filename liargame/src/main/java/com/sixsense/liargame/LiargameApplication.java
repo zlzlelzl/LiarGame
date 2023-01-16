54fc6b92c752967b8780d628f56fb5fae5784fa3
이상현 <dkrdj1000@naver.com>
2023-01-16 16:03:50 +0900

    init

--- liargame/src/main/java/com/sixsense/liargame/LiargameApplication.java
+++ liargame/src/main/java/com/sixsense/liargame/LiargameApplication.java
@@ -2,34 +2,12 @@ package com.sixsense.liargame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.sixsence.liargame")
 public class LiargameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LiargameApplication.class, args);
-    }
-
-    @Bean
-    public HttpMessageConverter<String> responseBodyConverter() {
-        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
-    }
-
-    @Bean
-    public CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
     }
 
 }
