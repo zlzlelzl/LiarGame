64a135c7608316844f47b6a523bf38a32fc3084a
sky_rest <sky_rest@naver.com>
2023-01-18 15:24:22 +0900

    문제점 추가

--- front-end/src/components/lobby/MainLobby.vue
+++ front-end/src/components/lobby/MainLobby.vue
@@ -1,31 +1,38 @@
 <template>
   <div class="lobby">
-    <div class="row" style="height:20%">
-        <lobby-header></lobby-header>
+    <!-- <div class="row" style="height: 20%; background-color: red"> -->
+    <div class="row" style="height: 20%">
+      <lobby-header></lobby-header>
     </div>
-    <div class="row" style="height:80%">
-        <lobby-main></lobby-main>
+    <!-- <div class="row" style="height: 80%; background-color: blue"> -->
+    <div class="row" style="height: 80%">
+      <lobby-main></lobby-main>
     </div>
-    <div class="row" style="height:20%">
-        <lobby-footer></lobby-footer>
+    <!-- <div class="row" style="height: 20%; background-color: yellow"> -->
+    <div class="row" style="height: 20%">
+      <lobby-footer></lobby-footer>
     </div>
   </div>
 </template>
 
 <script>
-import LobbyHeader from "@/components/lobby/LobbyHeader.vue"
-import LobbyMain from "@/components/lobby/LobbyMain.vue"
-import LobbyFooter from "@/components/lobby/LobbyFooter.vue"
+import LobbyHeader from "@/components/lobby/LobbyHeader.vue";
+import LobbyMain from "@/components/lobby/LobbyMain.vue";
+import LobbyFooter from "@/components/lobby/LobbyFooter.vue";
 
 export default {
-  components: { 
-    LobbyHeader ,LobbyMain ,LobbyFooter
-    },
-  name: "MainLobby",
-  created() {
-    
+  components: {
+    LobbyHeader,
+    LobbyMain,
+    LobbyFooter,
   },
-}
+  name: "MainLobby",
+  created() {},
+};
 </script>
 
-<style></style>
+<style>
+.lobby {
+  background-color: red;
+}
+</style>
