2fad62e96477882eb58db9b37da4410341297adb
charoon <wjswndud53@gmail.com>
2023-01-19 15:29:15 +0900

    security jwt 반환까지 구현

--- back-end/liargame/src/main/java/com/sixsense/liargame/api/controller/LoginLogoutController.java
+++ back-end/liargame/src/main/java/com/sixsense/liargame/api/controller/LoginLogoutController.java
@@ -6,19 +6,19 @@ import com.sixsense.liargame.common.model.response.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/test")
-public class UserController {
+public class LoginLogoutController {
     private final UserService userService;
 
     @PostMapping("/login")
     public TokenInfo login(@RequestBody UserDto userDto) {
         String id = userDto.getEmail();
         String password = userDto.getPassword();
+        System.out.println(id);
+        System.out.println(password);
         TokenInfo tokenInfo = userService.login(id, password);
         return tokenInfo;
     }
