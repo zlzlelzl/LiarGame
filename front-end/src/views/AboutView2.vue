db88efc30a6f2504acce97ccd0f8ecdfbf97f2fa
sky_rest <sky_rest@naver.com>
2023-01-25 09:57:01 +0900

    메인로비작업css 완료

---
+++ front-end/src/views/AboutView2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about" style="width:100%;height:100vh;">
+    
+    <div class="row">
+        <div class="col-3">
+        </div>
+        <div class="col-6">
+            <main-lobby style="width:100%;height:100vh;"></main-lobby>
+        </div>
+        <div class="col-3">
+        </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// import { mapState } from "vuex";
+import MainLobby from "@/components/lobby/MainLobby.vue";
+
+export default {
+    "name":"AboutView2",
+    components: {
+    MainLobby
+  }
+}
+
+</script>
