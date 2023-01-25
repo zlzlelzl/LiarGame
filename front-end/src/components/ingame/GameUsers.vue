9698756e9e1359f42d587ee4d56de5a7bc442412
김동현 <luis1119@naver.com>
2023-01-25 13:56:56 +0900

    인게임작업3

--- front-end/src/components/ingame/GameUsers.vue
+++ front-end/src/components/ingame/GameUsers.vue
@@ -1,53 +1,72 @@
 <template>
-  <div class="m-0 p-0 users">
-    <div class="m-0 p-0 row" style="height:40%;">
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
+    <div class="m-0 p-0 users">
+      <div class="m-0 p-0 row" style="height:40%;">
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              
+              <user-comp @click="toggleShow"></user-comp>
+              <div v-if="show">liar</div> 
+              
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+      </div>
+      <div class="m-0 p-0 row" style="height:10%;"></div>
+      <div class="m-0 p-0 row" style="height:40%;">
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+          <div class="m-0 p-0" style="width:20%">
+              <user-comp></user-comp>
+          </div>
+      </div>
     </div>
-    <div class="m-0 p-0 row" style="height:10%;"></div>
-    <div class="m-0 p-0 row" style="height:40%;">
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-        <div class="m-0 p-0" style="width:20%">
-            <user-comp></user-comp>
-        </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import UserComp from "@/components/ingame/UserComp.vue"
-
-export default {
-  components: { UserComp },
-  name: "GameUser",
-  created() {
-    
+  </template>
+  
+  <script>
+  import UserComp from "@/components/ingame/UserComp.vue"
+  
+  export default {
+    components: { UserComp },
+    name: "GameUser",
+  //   data: {
+  //     show: true
+  //   },
+  el:'#liar',
+  data(){
+      return{
+          show: false
+      }
   },
-}
-</script>
-
-<style></style>
+    created() {
+      
+    },
+    methods:{
+      toggleShow(){
+          this.show = !this.show;
+          
+      }
+    }
+  }
+  </script>
+  
+  <style></style>
