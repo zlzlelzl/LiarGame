9f29f9a942201b984312a1e9264ef5b299331131
이상현 <dkrdj1000@naver.com>
2023-02-01 15:03:32 +0900

    room

--- back-end/liargame/src/main/java/com/sixsense/liargame/common/model/Vote.java
+++ back-end/liargame/src/main/java/com/sixsense/liargame/common/model/Vote.java
@@ -1,12 +1,16 @@
 package com.sixsense.liargame.common.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @AllArgsConstructor
-public class Vote {
+@EqualsAndHashCode
+public class Vote implements Serializable {
     private Integer voter;
     @Setter
     private Integer target;
