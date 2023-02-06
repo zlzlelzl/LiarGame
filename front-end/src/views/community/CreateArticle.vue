a869b44dd05f9c2a4ec444c24f722c7ec4120b38
김동현 <luis1119@naver.com>
2023-02-06 13:50:42 +0900

    mypage2

---
+++ front-end/src/views/community/CreateArticle.vue
@@ -0,0 +1,62 @@
+<!-- <template>
+  <navbar />
+  <div class="CreateArticle">
+    <h1>dddd</h1>
+  </div>
+  
+</template> -->
+
+<!-- 
+<script>
+import navbar from "@/components/NavView.vue";
+export default {
+  name: "CreateArticle",
+  components: { navbar },
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+.CreateArticle {
+  background-image: url(../../assets/lobby/lobby.jpg);
+  background-repeat: no-repeat;
+  background-size:cover;
+  color: white;
+}
+</style> -->
+
+
+  
+  <script>
+  import navbar from "@/components/NavView.vue";
+
+  
+  export default {
+    name: "Community1",
+    components: { navbar },
+    data() {
+      return {
+        
+      }
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    methods: {
+      fnGetList() {
+        this.list = [
+          
+        ]
+      }
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
+  
