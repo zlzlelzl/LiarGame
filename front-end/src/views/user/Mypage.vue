3751b626e766b6902ac161bf113349ca5680a073
심호연 <weedsim@gmail.com>
2023-02-06 15:35:21 +0900

    마이페이지 디자인 임시

--- front-end/src/views/user/Mypage.vue
+++ front-end/src/views/user/Mypage.vue
@@ -1,23 +1,59 @@
 <template>
   <navbar />
   <div class="m-0 p-0">
-    <div class="m-0 p-0 mypagemain" style="height: 95vh">
-      <div class="m-0 p-0 row">
-        <table class="table">
+    <div class="m-0 p-0 mypage" style="height: 95vh; width: 100%; ">
+      <div class="m-0 p-0 row" style="display: flex; justify-content: center; align-items: center;">
+        <h1>마이페이지</h1>
+        <hr>
+        <h3>유저 정보</h3>
+        <table class="usertable thead-dark" style="margin: 0 auto;">
+          <tbody>
           <tr>
-            <td></td>
+            <th>유저 이메일 : </th>
+            <th>{{ email }}</th>
           </tr>
+          <tr>
+            <td style="color: white; margin-right: 1%;">유저 이름:</td>
+            <td><input type="text" v-model="username" v-bind:disabled="!isinputActive" /> <button id="namebtn" @click="namedupli">변경</button> </td>
+          </tr>
+          <tr>
+            <td style="color: white;">수정 비밀번호 입력 : </td>
+            <td><input type="text" v-model="modifyPW" /></td>
+          </tr>
+          <tr>
+            <td style="color: white;">수정 비밀번호 확인 : </td>
+            <td><input type="text" v-model="remodifyPW" /> <button id="pwbtn" @click="patchPW">수정</button></td>
+          </tr>
+        </tbody>
         </table>
-
-        <ul style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-          <li><h1 class="usermail">유저 이메일: {{ email }} </h1></li>
-          <li>유저 이름: <input type="text" v-model="username" v-bind:disabled="!isinputActive" placeholder="{{ name }}"/> <button id="namebtn" @click="namedupli">이름 변경</button> </li>
-          <li>수정 비밀번호 입력: <input type="text" v-model="modifyPW" /></li>
-          <li>수정 비밀번호 확인: <input type="text" v-model="remodifyPW" /> <button id="pwbtn" @click="patchPW">비밀 번호 수정</button></li>
-        </ul>
       </div>
-      <hr class="my-4">
+      <hr class="my-4 hist ">
       <!-- 전적확인란 -->
+      <h3>전적 정보</h3>
+      <table class="historytable thead-dark" style="margin: 0 auto;">
+        <thead>
+          <tr>
+            <th scope="col">normal</th>
+            <th scope="col">spy</th>
+          </tr>
+          <tr>
+            <td>{{ normalhistory }}</td>
+            <td>{{ spyhistory }}</td>
+          </tr>
+          <tr v-for="(item, index) in normallist" :key="index">
+            <td>{{ item.result }}</td>
+            <td>{{ item.role }}</td>
+            <td>{{ item.playedTime }}</td>
+            <td>{{ item.users.join(', ') }}</td>
+            <td v-if="spylist[index]">{{ spylist[index].result }}</td>
+            <td v-if="spylist[index]">{{ spylist[index].role }}</td>
+            <td v-if="spylist[index]">{{ spylist[index].playedTime }}</td>
+            <td v-if="spylist[index]">{{ spylist[index].users.join(', ') }}</td>
+          </tr>
+        </thead>
+      </table>
+      <!-- <hr class="my-4 reward">
+      리워드란 -->
     </div>
   </div>
 </template>
@@ -30,7 +66,7 @@ export default {
   components: { navbar },
   data() {
     return {
-      name: null,
+      username: null,
       email: null,
       pwd: null,
       chkpwd: null,
@@ -38,6 +74,17 @@ export default {
       remodifyPW: "",
       modifyPW: "",
       isinputActive: false,
+      normalhistory: null, //노말모드 전체 승패
+      spyhistory: null, //스파이모드 전체 승패
+      normalwin: null,  //노말모드 승수
+      normallose: null,   //노말모드 패수
+      normalplayed:null,   //노말모드 플레이한 수
+      spywin: null,   //스파이모드 승수
+      spylose: null,    //스파이모드 패수
+      spyplayed: null,    //스파이모드 플레이한 수
+      normallist: null,   //노말모드 리스트
+      spylist: null,   //스파이모드 리스트
+
     };
   },
   watch: {
@@ -51,6 +98,7 @@ export default {
   setup() {},
   created() {
     this.userInfo();
+    
   },
   mounted() {},
   methods: {
@@ -58,20 +106,74 @@ export default {
       axios({
         method: "get",
         url: `${this.API_URL}/users`,
-        // headers: {
-        //   Authorization: `Bearer ${this.$store.state.accessToken}`,
-        // },
-        data: {
-          accessToken: this.$store.state.accessToken,
+        headers: {
+          Authorization: `Bearer ${this.$store.state.accessToken}`,
         },
       })
         .then((res) => {
+          this.name = res.data.name;
+          this.email = res.data.email;
           // console.log(res.data.accessToken);
-          // console.log(res.data.refreshToken);
           console.log(res);
         })
         .catch((err) => {
-          console.log("실패");
+          console.log("유저정보실패");
+          console.log(err);
+        });
+        axios({
+          method: "get",
+          url: `${this.API_URL}/history/normal`,
+          headers: {
+            Authorization: `Bearer ${this.$store.state.accessToken}`,
+          },
+        })
+        .then((res) => {
+          this.normallist = res;
+          let countwin = 0;
+          let countlose = 0;
+          for(let i = 0; i<res.length; i++){
+            if(res[i].result === "win"){
+              countwin++;
+            }
+            else{
+              countlose++;
+            }
+          }
+          this.normalwin = countwin;
+          this.normallose = countlose;
+          this.normalplayed = res.length;
+          this.normalhistory = this.normalplayed + '전 ' + this.normalwin + '승 ' + this.normallose + '패';
+        })
+        .catch((err) => {
+          console.log("노말모드실패");
+          console.log(err);
+        });
+        axios({
+          method: "get",
+          url: `${this.API_URL}/history/spy`,
+          headers: {
+            Authorization: `Bearer ${this.$store.state.accessToken}`,
+          },
+        })
+        .then((res) => {
+          this.spylist = res;
+          let countwin = 0;
+          let countlose = 0;
+          for(let i = 0; i < res.length; i++){
+            if(res[i].result === "win"){
+              countwin++;
+            }
+            else{
+              countlose++;
+            }
+          }
+          this.spywin = countwin;
+          this.spylose = countlose;
+          this.spyplayed = res.length;
+          this.spyhistory = this.spyplayed + '전 ' + this.spywin + '승 ' + this.spylose + '패';
+        })
+        .catch((err) => {
+          console.log("스파이모드실패");
           console.log(err);
         });
     },
@@ -129,10 +231,24 @@ export default {
 </script>
 
 <style scoped>
-  .mypagemain {
-    background-image: url(../../assets/home/maintop.jpg);
+  .mypage {
+    background-image: url(../../assets/lobby/lobby.png);
+    background-size: 102vw 100vh;
+    background-repeat: no-repeat;
+    background-position-x: -10px;
+    color:white;
   }
   hr.my-4 {
     border: 1px solid white;
   }
+  .usertable{
+    border-collapse: separate;
+    border-spacing: 1vh;
+    border: 1px solid white;
+  }
+  th, td{
+    border: 1px solid white;
+  }
+  
+  
 </style>
