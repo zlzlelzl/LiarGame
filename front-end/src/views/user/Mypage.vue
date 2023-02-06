ff522fbcf0fb844d81d84e6174ff1ef86eb81089
김동현 <luis1119@naver.com>
2023-02-06 10:55:43 +0900

    mypage틀

--- front-end/src/views/user/Mypage.vue
+++ front-end/src/views/user/Mypage.vue
@@ -1,20 +1,35 @@
 <template>
-  <div>
+  <navbar/>
+  <div class = "mypage" style="width: 100%; height: 95vh">
+    <h1>마이 페이지</h1>
+    <hr>
+    <h3>유저 정보</h3>
     <ul>
       <li>유저 이메일: {{ email }}</li>
-      <li>유저 이름: <input type="text" v-model="name" /></li>
-      <li>현재 비밀번호 입력: <input type="text" /></li>
+      <li>유저 이름: <input type="text" v-model="name" /><button type="submit" class="btn btn-secondary mb-3 btn-sm">변경</button></li>
+      <li>현재 비밀번호 입력: <input type="text" /><button type="submit" class="btn btn-secondary mb-3 btn-sm">제출</button></li>
       <li>수정 비밀번호 입력: <input type="text" /></li>
-      <li>수정 비밀번호 확인: <input type="text" /></li>
+      <li>수정 비밀번호 확인: <input type="text" /><button type="submit" class="btn btn-secondary mb-3 btn-sm">확인</button></li>
     </ul>
+    <hr>
+    <h3>전적 정보</h3>
+    <ul>
+      <li>일반 모드 : 113전 61승 52패</li>
+      <li>스파이 모드 : 90전 51승 39패</li>
+    </ul>
+    <hr>
+    <h3>리워드</h3>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import navbar from "@/components/NavView.vue"
 
 export default {
-  components: {},
+  components: {
+    navbar
+  },
   data() {
     return {
       name: null,
@@ -55,4 +70,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.mypage {
+  background-image: url(../../assets/lobby/lobby.png);
+  background-size: 102vw 100vh;
+  background-repeat: no-repeat;
+  background-position-x: -10px;
+  color:white;
+}</style>
