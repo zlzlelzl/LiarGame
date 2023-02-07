weedsim/Mirroring_Common

레디스 삭제

back-end/liargame/src/main/java/com/sixsense/liargame/api/controller/RoomController.java
@@ -31,21 +31,21 @@ public class RoomController {
     }
 
     @PatchMapping("/{roomId}/enter")
-    public ResponseEntity<?> enter(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Long roomId) {
+    public ResponseEntity<?> enter(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Integer roomId) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         roomService.enter(userId, roomId);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{roomId}/exit")
-    public ResponseEntity<?> exit(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Long roomId) {
+    public ResponseEntity<?> exit(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Integer roomId) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         roomService.exit(userId, roomId);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{roomId}")
-    public ResponseEntity<?> changeSetting(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Long roomId, @RequestBody SettingDto settingDto) {
+    public ResponseEntity<?> changeSetting(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Integer roomId, @RequestBody SettingDto settingDto) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         settingDto.setId(roomId);
         roomService.changeSetting(userId, settingDto);

zlzlelzl/LiarGame

back-end/liargame/src/main/java/com/sixsense/liargame/api/controller/RoomController.java
@@ -31,21 +31,21 @@ public class RoomController {
     }
 
     @PatchMapping("/{roomId}/enter")
-    public ResponseEntity<?> enter(@RequestHeader(name = JwtProperties.AUTHORIZATION) String accessToken, @PathVariable Long roomId) {
+    public ResponseEntity<?> enter(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Long roomId) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         roomService.enter(userId, roomId);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{roomId}/exit")
-    public ResponseEntity<?> exit(@RequestHeader(name = JwtProperties.AUTHORIZATION) String accessToken, @PathVariable Long roomId) {
+    public ResponseEntity<?> exit(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Long roomId) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         roomService.exit(userId, roomId);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{roomId}")
-    public ResponseEntity<?> changeSetting(@RequestHeader(name = JwtProperties.AUTHORIZATION) String accessToken, @PathVariable Long roomId, @RequestBody SettingDto settingDto) {
+    public ResponseEntity<?> changeSetting(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @PathVariable Long roomId, @RequestBody SettingDto settingDto) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         settingDto.setId(roomId);
         roomService.changeSetting(userId, settingDto);
@@ -53,7 +53,7 @@ public class RoomController {
     }
 
     @PostMapping
-    public ResponseEntity<RoomTokenResp> create(@RequestHeader(name = JwtProperties.AUTHORIZATION) String accessToken, @RequestBody RoomReq roomReq) {
+    public ResponseEntity<RoomTokenResp> create(@RequestHeader(name = JwtProperties.AUTHORIAZATION) String accessToken, @RequestBody RoomReq roomReq) {
         Long userId = jwtTokenProvider.getUserId(accessToken);
         RoomTokenResp roomTokenResp;
         try {